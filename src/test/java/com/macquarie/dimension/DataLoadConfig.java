2e0320b9d60c3e7dcfef22e39eaa93f273527115
hopeng <haopeng.huang@gmail.com>
2018-04-09 08:44:34 +1000

    SampleBootRunnerTest works with hbm2ddl create-drop and DataLoadConfig (jpa)

--- src/test/java/com/macquarie/dimension/DataLoadConfig.java
+++ src/test/java/com/macquarie/dimension/DataLoadConfig.java
@@ -1,21 +1,25 @@
 package com.macquarie.dimension;
 
+import com.macquarie.dimension.domain.TestEntity;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Map;
 
 /**
  * Created by hopeng on 9/4/18.
  */
-@Configuration
+@TestConfiguration
 public class DataLoadConfig {
     @Bean
     public JobLauncherTestUtils jobLauncherTestUtils() {
@@ -25,9 +29,31 @@ public class DataLoadConfig {
     @Autowired
     DataSource dimensionDataSource;
 
+    @Autowired
+    EntityManagerFactory dimensionEntityManagerFactory;
+
     @EventListener({ContextRefreshedEvent.class})
     void contextRefreshedEvent() {
         System.out.println("a context refreshed event happened");
+        loadData();
+    }
+
+    private void loadData() {
+        EntityManager entityManager = dimensionEntityManagerFactory.createEntityManager();
+
+        TestEntity entity = new TestEntity();
+        entity.setId("1");
+        entity.setAge(11);
+        entity.setUpdatedBy(ZonedDateTime.now());
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(entity);
+        entityManager.flush();
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
+    private void loadDataJdbc() {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dimensionDataSource);
         jdbcTemplate.update("insert into test_entity(id, age) VALUES ('1', 11)");
         List<Map<String, Object>> result = jdbcTemplate.queryForList("select * from test_entity");
--- src/test/java/com/macquarie/dimension/SampleBootRunnerTest.java
+++ src/test/java/com/macquarie/dimension/SampleBootRunnerTest.java
@@ -3,12 +3,15 @@ package com.macquarie.dimension;
 import org.junit.Test;
 import org.springframework.boot.SpringApplication;
 
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@Import(DataLoadConfig.class)
 public class SampleBootRunnerTest {
 
     @Test
     public void test() {
         // run job
-        SpringApplication.run(DimensionEntry.class);
+        SpringApplication.run(new Class[] {DimensionEntry.class, DataLoadConfig.class}, new String[0]);
 
     }
 }
